a0ee26353ece1122c9b69a6f2506384e94f1c97b
anuwatsukk-byte <anuwat.sukk@bumail.net>
2026-03-10 16:33:39 +0700

    Angular and Torque

---
+++ Assets/Scripts/TorqueRotate.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorqueRotate : MonoBehaviour
+{
+    public float torquePower = 6f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(Vector3.forward * torquePower);
+        }
+
+    }
+}
